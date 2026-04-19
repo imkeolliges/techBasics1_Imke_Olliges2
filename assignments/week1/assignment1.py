ced8442539f063b99205c55dd27b2a17c2af0052
imkeolliges <imke.olli@gmail.com>
2026-04-20 01:37:30 +0200

    Create assignment1.py

---
+++ assignments/week1/assignment1.py
@@ -0,0 +1,9 @@
+print("""___  ___ ----------------___   ___
+`-....-´    *` *** ´*     `-....-´
+  ....     *  -`♡´-   *     ....
+  ....       /)__ (/        ....
+   ...      ( ().() )       ....""")
+
+# My approach to the Ascii art was more or less trial and error.
+# Somehow my doodling started looking like a pillar, so I decided to do two pillars 
+# with an animal that looks like a mixture of a bunny and a cat.
